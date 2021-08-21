cf9453d6968bf0e5259a8071a16c9e5feaae09d8
jangdawit <jangdawit@gmail.com>
2021-08-21 16:43:20 +0900

    방 입장 이벤트 broadcast, 채팅 로그 redis에 기록

--- src/main/java/co/mobile/b/server/config/stomp/SocketEventHandler.java
+++ src/main/java/co/mobile/b/server/config/stomp/SocketEventHandler.java
@@ -1,21 +1,26 @@
 package co.mobile.b.server.config.stomp;
 
+import co.mobile.b.server.socket.SocketController;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class SocketEventHandler {
+    private final SocketController socketController;
 
     /**
      * 커넥 리스너
@@ -26,9 +31,17 @@ public class SocketEventHandler {
     public void handleWebSocketConnectListener(SessionConnectedEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
 
-        //GenericMessage msg = (GenericMessage) headerAccessor.getMessageHeaders().get("simpConnectMessage");
+        GenericMessage msg = (GenericMessage) headerAccessor.getMessageHeaders().get("simpConnectMessage");
+        Map<String, List> nativeHeaders = (Map<String, List>) msg.getHeaders().get("nativeHeaders");
 
         log.info("handleWebSocketConnectListener. Session ID : [{}]", headerAccessor.getSessionId());
+
+        // 헤더로부터 초대코드, 유저명, 포지션 저장
+        String inviteCode = (String)nativeHeaders.get("inviteCode").get(0);
+        String username = (String) nativeHeaders.get("username").get(0);
+        int positionType = Integer.parseInt((String)nativeHeaders.get("positionType").get(0));
+
+        socketController.enterBroadcast(inviteCode,username,positionType);
     }
 
 
